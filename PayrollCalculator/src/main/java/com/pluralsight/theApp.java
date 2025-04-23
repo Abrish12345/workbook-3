adc6283487372d98ba0500b811867d2379805695
Abraham Belayhun <abrahambelayhun@gmail.com>
2025-04-23 16:15:39 -0500

    writer

--- PayrollCalculator/src/main/java/com/pluralsight/theApp.java
+++ PayrollCalculator/src/main/java/com/pluralsight/theApp.java
@@ -1,16 +1,25 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
+import java.util.Scanner;
 
 public class theApp {
     public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        // Prompt for input and output filenames.
+        System.out.print("Enter the name of the employee file to process:" );
+        String inputFileName= scanner.nextLine();
+        System.out.print("Enter the name of the payroll file to create:" );
+        String outputFileName= scanner.nextLine();
 
         try {
             //fileReader
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            FileReader fileReader = new FileReader("src/main/resources/" + inputFileName);
             //Wrap the fileReader with bufferedreader.
             BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            BufferedWriter writer= new BufferedWriter(new FileWriter("src/main/resources/" + outputFileName));
+            writer.write("id|name|gross pay\n");
 
             //create variable name input that holds each line from the file
             String inpute;
@@ -23,14 +32,22 @@ public class theApp {
                 }
                 Employee theEmployee = new Employee(Integer.parseInt(inputParts[0]), inputParts[1], Double.parseDouble(inputParts[2]), Double.parseDouble(inputParts[3]));
 
+                String outputLine= theEmployee.getEmployeeId() +"|" + theEmployee.getName() +"|"+ theEmployee.getGrossPay();
+                writer.write(outputLine + "\n");
+
+
+                 /*
                 System.out.printf("%d - %s - $%.2f\n", theEmployee.getEmployeeId(), theEmployee.getName(), theEmployee.getGrossPay());
-                /*
+
                 System.out.println("ID: " + theEmployee.getEmployeeId());
                 System.out.println("Name: " + theEmployee.getName());
                 System.out.println("Gross Pay: $" + theEmployee.getGrossPay());
                 System.out.println("--------------------------");
                 */
             }
+            writer.close();
+            bufferedReader.close();
+            System.out.println("Payroll file created: " + outputFileName);
 
         }catch (Exception e){
             throw new RuntimeException(e);
